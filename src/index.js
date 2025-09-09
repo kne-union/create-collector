4090617161b8117d21f3d4e561c49cd90c461454
Linzp <sunandmoon001@163.com>
2025-09-09 15:16:09 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const createCollector = (callback, options) => {
   // 有maxLength和maxTime两种模式，默认为maxTime模式，
   // 即：maxTime时间间隔内所有消息一次性触发回调，如果maxTime内没有消息则不触发
   // maxLength模式则在消息数量达到maxLength时触发回调
-  let messages = [],
-    executorTime;
+  const isMaxLengthMode = Number.isInteger(options.maxLength) && options.maxLength > 0;
+  let messages = [];
   const executorCallback = async () => {
     try {
       messages.length > 0 && (await callback(messages));
@@ -13,20 +13,26 @@ const createCollector = (callback, options) => {
       console.error(e);
     }
   };
+
+  const timer =
+    !isMaxLengthMode &&
+    setInterval(() => {
+      executorCallback();
+    }, options.maxTime);
+
   const api = async data => {
     messages.push(data);
-    executorTime = new Date();
-    if (Number.isInteger(options.maxLength) && options.maxLength > 0 && messages.length >= options.maxLength) {
+    if (isMaxLengthMode && messages.length >= options.maxLength) {
       await executorCallback();
-      return;
-    }
-    if (Date.now() - executorTime >= options.maxLength.maxTime) {
-      await executorCallback();
-      return;
     }
   };
 
   api.all = executorCallback;
+
+  api.close = async () => {
+    timer && clearInterval(timer);
+    await executorCallback();
+  };
 
   return api;
 };
